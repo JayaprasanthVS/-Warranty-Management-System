e26bff7bd4b08ba0e82483b35660dff40d1e155b
JAYAPRASANTH <songofnone@gmail.com>
2026-05-31 13:19:47 +0530

    Fix base path for Vercel

--- warranty-ui/vite.config.js
+++ warranty-ui/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/-Warranty-Management-System/", // Matches your GitHub repo name
+  base: "/", // Matches your GitHub repo name
 });
